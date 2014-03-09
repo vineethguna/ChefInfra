f0ca8a93067278763541b31371e764f50b54c2e2
vineethguna9050 <vineeth.guna@kony.com>
2014-03-09 18:30:11 +0530

    Deleted README files and updated knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -2,9 +2,9 @@ current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
 node_name                "vineethguna"
-client_key               "#{current_dir}/vineethguna.pem"
+client_key               "/var/lib/jenkins/.chef/vineethguna.pem"
 validation_client_name   "vineeth-validator"
-validation_key           "#{current_dir}/vineeth-validator.pem"
+validation_key           "/var/lib/jenkins/.chef/vineeth-validator.pem"
 chef_server_url          "https://api.opscode.com/organizations/vineeth"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
