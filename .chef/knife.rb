740a94ca233e303ef158d9eb070b33b2653e0451
vineethguna9050 <vineeth.guna@kony.com>
2014-03-22 15:01:13 +0530

    Changed knife.rb

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,11 +1,11 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "vineethguna93"
-client_key               "/var/lib/jenkins/.chef/vineethguna93.pem"
-validation_client_name   "vineeth-validator"
-validation_key           "/var/lib/jenkins/.chef/vineethguna-validator.pem"
-chef_server_url          "https://api.opscode.com/organizations/vineethguna"
+node_name                "vineethguna1993"
+client_key               "/var/lib/jenkins/.chef/vineethguna1993.pem"
+validation_client_name   "chefinfra-validator"
+validation_key           "/var/lib/jenkins/.chef/chefinfra-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/chefinfra"
 cache_type               'BasicFile'
 cookbook_path            ["#{current_dir}/../cookbooks"]
 
