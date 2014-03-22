2b1add0b52d78d0ea4d380849c1ea8bfae1f1c4b
vineethguna9050 <vineeth.guna@kony.com>
2014-03-22 14:32:31 +0530

    Changed ssh keys

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -1,11 +1,11 @@
 current_dir = File.dirname(__FILE__)
 log_level                :info
 log_location             STDOUT
-node_name                "vineethguna"
-client_key               "/var/lib/jenkins/.chef/vineethguna.pem"
+node_name                "vineethguna93"
+client_key               "/var/lib/jenkins/.chef/vineethguna93.pem"
 validation_client_name   "vineeth-validator"
-validation_key           "/var/lib/jenkins/.chef/vineeth-validator.pem"
-chef_server_url          "https://api.opscode.com/organizations/vineeth"
+validation_key           "/var/lib/jenkins/.chef/vineethguna-validator.pem"
+chef_server_url          "https://api.opscode.com/organizations/vineethguna"
 cache_type               'BasicFile'
 cache_options( :path => "#{ENV['HOME']}/.chef/checksums" )
 cookbook_path            ["#{current_dir}/../cookbooks"]
